1befef62b802ddc987ee1f16d6350a1fc25f9ae1
melisalsan <melis.alsan@hisarschool.k12.tr>
2024-01-14 16:11:25 +0300

    yeni drivebase kodu ve cok cok tatli sadece canid isini cozmem lazim sanirim wiring sıkıntısı

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,140 +5,93 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import frc.robot.autonomous.Autonomous;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Pneumatics;
-import frc.robot.subsystems.Subsystem;
-import frc.robot.subsystems.Superstructure;
-import frc.robot.subsystems.drivebase.Drivebase;
-import frc.robot.subsystems.shooter.Shooter;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.lib.config.CTREConfigs;
 
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
-    private final Autonomous autonomous = Autonomous.getInstance();
+  public static CTREConfigs ctreConfigs;
+  private Command m_autonomousCommand;
+  private RobotContainer m_robotContainer;
 
-    private final Subsystem[] subsystems = new Subsystem[] {
-            Infrastructure.getInstance(),
-            Shooter.getInstance(),
-            Intake.getInstance(),
-            Superstructure.getInstance(),
-            Pneumatics.getInstance(),
-            Drivebase.getInstance()
-    };
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    ctreConfigs = new CTREConfigs();
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+  }
 
-    private boolean enabled = false;
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
 
-    /**
-     * Code that runs once when the robot code starts
-     */
-    @Override
-    public void robotInit() {
-        autonomous.robotInit();
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
 
-        for (Subsystem subsystem : subsystems) {
-            subsystem.robotInit();
-        }
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
     }
+  }
 
-    /**
-     * Code that runs once per robot tick
-     */
-    @Override
-    public void robotPeriodic() {
-        for (Subsystem subsystem : subsystems) {
-            subsystem.robotPeriodic();
-        }
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
 
-        Infrastructure.getInstance().enabled = isEnabled();
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
+  }
 
-    /**
-     * Code that runs once when the autonomous period starts
-     */
-    @Override
-    public void autonomousInit() {
-        autonomous.autonomousInit();
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
 
-        if (!enabled) {
-            for (Subsystem subsystem : subsystems) {
-                subsystem.enabledInit();
-            }
-            enabled = true;
-        }
-    }
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
 
-    /**
-     * Code that runs once per autonomous tick
-     */
-    @Override
-    public void autonomousPeriodic() {
-      autonomous.autonomousPeriodic();
-    }
-
-    /**
-     * Code that runs once when the teleop period starts
-     */
-    @Override
-    public void teleopInit() {
-      if (!enabled) {
-          for (Subsystem subsystem : subsystems) {
-              subsystem.enabledInit();
-          }
-          enabled = true;
-      }
-
-      for (Subsystem subsystem : subsystems) {
-          subsystem.teleopInit();
-      }
-    }
-
-    /**
-     * Code that runs once per teleop tick
-     */
-    @Override
-    public void teleopPeriodic() {
-      for (Subsystem subsystem : subsystems) {
-          subsystem.teleopPeriodic();
-      }
-    }
-
-    /**
-     * Code that runs once when the robot is disabled
-     */
-    @Override
-    public void disabledInit() {
-        enabled = false;
-        for (Subsystem subsystem : subsystems) {
-            subsystem.disabledInit();
-        }
-    }
-
-    /**
-     * Code that runs once per disabled tick
-     */
-    @Override
-    public void disabledPeriodic() {
-        for (Subsystem subsystem : subsystems) {
-            subsystem.disabledPeriodic();
-        }
-    }
-
-    /**
-     * Code that runs once when the robot is put into test mode
-     */
-    @Override
-    public void testInit() {
-        for (Subsystem subsystem : subsystems) {
-            subsystem.testInit();
-        }
-    }
-
-    /**
-     * Code that runs once per test tick
-     */
-    @Override
-    public void testPeriodic() {
-        for (Subsystem subsystem : subsystems) {
-            subsystem.testPeriodic();
-        }
-    }
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 }
--- src/main/java/frc/robot/constants/PHChannel.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.constants;
-
-public class PHChannel {
-    public static final int FRONT_INTAKE_DEPLOY = 0;
-    public static final int FRONT_INTAKE_RETRACT = 1;
-    public static final int BACK_INTAKE_DEPLOY = 2;
-    public static final int BACK_INTAKE_RETRACT = 3;
-}
